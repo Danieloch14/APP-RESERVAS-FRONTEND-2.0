476fef03b90a2fc0177415f659205840d1075ff4
Timoteo Camuendo <timo_teo.c@hotmail.com>
2023-11-20 10:13:45 -0500

    feat: environment files,

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 import { API_URL, API_VERSION } from "../constants/environment.const";
 
-export const environment = {
+export const environmentDev = {
   production: false,
   [API_URL]: 'http://localhost:8083/api',
   [API_VERSION]: 'v1'
