a1badcc21f3149b0899c11f4fe0984719dcccbfa
Timoteo Camuendo <59524647+Timo-Teo@users.noreply.github.com>
2023-11-20 10:57:19 -0500

    feat: nav, notification bell

--- src/app/private/components/notification-bell/notification-bell.component.ts
+++ src/app/private/components/notification-bell/notification-bell.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatMenuModule} from "@angular/material/menu";
 
 @Component({
   selector: 'app-notification-bell',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatMenuModule],
   templateUrl: './notification-bell.component.html',
   styleUrls: ['./notification-bell.component.scss']
 })
