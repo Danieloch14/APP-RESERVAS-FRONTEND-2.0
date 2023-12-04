277a92bc1673c7654f50dd62ae8ac4e6d9e921e2
Alejandra Oña <alejandra.onadelatorre@epn.edu.ec>
2023-12-03 20:12:27 -0500

    feat: access request and profile settings

--- src/app/models/Reservation.ts
+++ src/app/models/Reservation.ts
@@ -2,11 +2,11 @@ import { Resource } from "./Resource";
 import { User } from "./User";
 
 
-export class Reservation{
-    idReservation !: number;
-    idResource !: Resource;
-    idUser !: User;
-    startDate !: Date;
-    endDate !: Date;
-    status !: string;
+export interface Reservation{
+    idReservation : number;
+    idResource : Resource;
+    idUser : User;
+    startDate : Date;
+    endDate : Date;
+    status : string;
 }
---
+++ src/app/private/admin/pages/access-request/access-request.component.ts
@@ -0,0 +1,138 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { RequestService } from '../../services/request.service';
+import { Request } from 'src/app/models/Request';
+
+@Component({
+  selector: 'app-access-request',
+  templateUrl: './access-request.component.html',
+  styleUrls: ['./access-request.component.scss']
+})
+export class AccessRequestComponent {
+
+  
+  displayedColumns: string[] = [
+    'dateRequest',
+    'requesterName',
+    'requesterLastname',
+    'requesterEmail',
+    'status',
+    'actions'
+  ];
+
+  dataSource!: MatTableDataSource<Request>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  listRequest!: Request[];
+  successAccepted: boolean = false;
+  successDenied: boolean = false;
+
+  constructor(
+    private requestService: RequestService
+  ) {  
+    this.listRequest = [
+      {
+        dateRequest: new Date(20, 10, 10),
+        requesterName: 'Juan',
+        requesterLastname: 'Perez',
+        requesterEmail: 'juan.perez@netlife.ec',
+        status: 'Pendiente'
+      },
+      {
+        dateRequest: new Date(20, 10, 10),
+        requesterName: 'Juan',
+        requesterLastname: 'Perez',
+        requesterEmail: 'juan.perez@netlife.ec',
+        status: 'Pendiente'
+      },
+      {
+        dateRequest: new Date(),
+        requesterName: 'Juan',
+        requesterLastname: 'Perez',
+        requesterEmail: 'juan.perez@netlife.ec',
+        status: 'Aceptada'
+      },
+      {
+        dateRequest: new Date(21, 10, 10),  
+        requesterName: 'Juan',
+        requesterLastname: 'Perez',
+        requesterEmail: 'juan.perez@netlife.ec',
+        status: 'Rechazada'
+      },
+      {
+        dateRequest: new Date(22, 10, 10),
+        requesterName: 'Juan',
+        requesterLastname: 'Perez',
+        requesterEmail: 'juan.perez@netlife.ec',
+        status: 'Pendiente'
+      },
+      {
+        dateRequest: new Date(23, 10, 10),
+        requesterName: 'Juan',
+        requesterLastname: 'Perez',
+        requesterEmail: 'juan.perez@netlife.ec',
+        status: 'Pendiente'
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+    // this.requestService.getAll().subscribe((requests) => {
+    //   console.log(requests)
+    //   this.listRequest = requests
+      this.dataSource = new MatTableDataSource(this.listRequest);
+
+    // },
+    // (error) =>{
+    //   console.log(error);
+    // })
+
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  onAccept(row: Request) {
+    this.successAccepted = true;
+
+      setTimeout(() => {
+        this.successAccepted = false;
+        this.ngOnInit();
+      }, 2000);
+  }
+
+  onDenied(row: Request){
+    // this.resourceService.delete(row.idResource).subscribe((response) => {
+    //   console.log(response);
+    //   this.successDelete = true;
+
+    //   setTimeout(() => {
+    //     this.successDelete = false;
+    //     this.ngOnInit();
+    //   }, 2000);
+    // },
+    // (error) =>{
+    //   console.log(error);
+    // })
+    this.successDenied = true;
+
+      setTimeout(() => {
+        this.successDenied = false;
+        this.ngOnInit();
+      }, 2000);
+  }
+}
